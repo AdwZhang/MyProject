a9bb5939327f8f05fc89de019cce4b933603e1f7
张振华 <hellozzhgg@163.com>
2020-09-09 19:09:35 +0800

    补充

--- DesignPattern/Observer/Observer/Subject.h
+++ DesignPattern/Observer/Observer/Subject.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "Observer.h"
 class Subject {
+public:
+	void addObserver(Observer* observer);
+	void removeObserver(Observer* observer);
+
 private:
 	Observer* observers_[MAX_OBSERVERS];
 };
